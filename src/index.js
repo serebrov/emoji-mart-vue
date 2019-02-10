94571356a708470a79c34c72cb8612717dc258a9
Boris Serebrov <serebrov@gmail.com>
2019-02-10 16:41:52 +0200

    Remove commented out code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import emojiIndex from './utils/emoji-index/emoji-index'
 import store from './utils/store'
 import { uncompress } from './utils/data'
 import frequently from './utils/frequently'
@@ -12,5 +11,4 @@ export {
 } from './components'
 
 export { default as NimbleEmojiIndex } from './utils/emoji-index/nimble-emoji-index'
-// export { emojiIndex, uncompress, store, frequently }
 export { uncompress, store, frequently }
