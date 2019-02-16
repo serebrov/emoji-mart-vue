80db4e3d03d991ecae325f067f13964b9bac7eff
Boris Serebrov <serebrov@gmail.com>
2019-02-16 19:13:46 +0200

    Add css loaders for docs build.

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -35,6 +35,10 @@ var config = {
         ]
       },
       {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader", "postcss-loader"]
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
