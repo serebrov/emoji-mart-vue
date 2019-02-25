e6c40fed50700264831a2bc8899a83aeae477504
Boris Serebrov <serebrov@gmail.com>
2019-02-25 23:19:37 +0200

    Readd picker test

---
+++ spec/picker-spec.js
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils'
+
+// import data from '../data/all.json'
+import { NimblePicker } from '../src/components'
+
+// const { click } = TestUtils.Simulate
+
+/*
+const {
+  renderIntoDocument,
+  scryRenderedComponentsWithType,
+  findRenderedComponentWithType,
+} = TestUtils
+*/
+
+/*
+const render = (props = {}) => {
+  const defaultProps = { data }
+  return renderIntoDocument(<NimblePicker {...defaultProps} {...props} />)
+}
+*/
+
+describe('NimblePicker', () => {
+  const picker = mount(NimblePicker)
+
+  it('works', () => {
+    console.log(picker.html())
+    expect(picker.html()).toContain('test')
+  })
+
+  /*
+  describe('categories', () => {
+    it('shows 10 by default', () => {
+      subject = render()
+      expect(subject.categories.length).toEqual(10)
+    })
+
+    it('will not show some based upon our filter', () => {
+      subject = render({ emojisToShowFilter: (unified) => false })
+      expect(subject.categories.length).toEqual(2)
+    })
+
+    it('maintains category ids after it is filtered', () => {
+      subject = render({ emojisToShowFilter: (emoji) => true })
+      const categoriesWithIds = subject.categories.filter(
+        (category) => category.id,
+      )
+      expect(categoriesWithIds.length).toEqual(10)
+    })
+  })
+  */
+})
