e523c345a77b29437ec7cc7b5993f3b82c546796
Boris Serebrov <serebrov@gmail.com>
2019-02-26 18:24:26 +0200

    Reorder tests

--- spec/picker-spec.js
+++ spec/picker-spec.js
@@ -49,34 +49,44 @@ describe('NimblePicker', () => {
     expect(picker.html()).toContain('woman-gesturing-ok"')
   })
 
-  describe('categories', () => {
-    it('shows 5 by default', () => {
-      // Due to the virtual scroller, not all the categories
-      // are rendered at once
-      expect(picker.findAll(Category).length).toBe(5)
-    })
+  it('shows 5 categories by default', () => {
+    // Due to the virtual scroller, not all the categories
+    // are rendered at once
+    expect(picker.findAll(Category).length).toBe(5)
+  })
 
-    /*
-    it('will not show some based upon our filter', () => {
-      let index = new EmojiIndex(
-        data, { emojisToShowFilter: (unified) => false })
-      const picker = mount(Picker, {
-        propsData: {
-          data: index
-        }
-      })
-      expect(picker.findAll(Category).length).toBe(2)
-    })
-    */
+})
 
-    /*
-    it('maintains category ids after it is filtered', () => {
-      subject = render({ emojisToShowFilter: (emoji) => true })
-      const categoriesWithIds = subject.categories.filter(
-        (category) => category.id,
-      )
-      expect(categoriesWithIds.length).toEqual(10)
-    })
-    */
+/*
+describe('categories', () => {
+  let index = new EmojiIndex(data, {
+    emojisToShowFilter: (emoji) => {
+      return emoji.short_names[0].match(/[0-9+-]+/) !== null
+    }
+  })
+  // let index = new EmojiIndex(data)
+  const picker = mount(Picker, {
+    propsData: {
+      data: index,
+      // Set idle emoji (in preview) to "+1"
+      emoji: '+1'
+    }
+  })
+
+  it('will not show some based upon our filter', () => {
+    expect(picker.findAll(Category).length).toBe(5)
+    // Filter out all emojis, except numeric like +1
+    // // debugger;
+    // // let result = picker.findAll(Category)[0]
+    // expect(picker.findAll(Category).length).toBe(2)
+  })
+
+  it('maintains category ids after it is filtered', () => {
+    subject = render({ emojisToShowFilter: (emoji) => true })
+    const categoriesWithIds = subject.categories.filter(
+      (category) => category.id,
+    )
+    expect(categoriesWithIds.length).toEqual(10)
   })
 })
+*/
