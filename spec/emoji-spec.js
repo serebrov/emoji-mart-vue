67b41548f61d96a5616149d03a7f0a990f2807b2
Boris Serebrov <serebrov@gmail.com>
2019-03-13 19:12:12 +0200

    More tests for emoji component.

--- spec/emoji-spec.js
+++ spec/emoji-spec.js
@@ -1,10 +1,11 @@
 import { mount } from '@vue/test-utils'
 
 import data from '../data/all.json'
-// import { EmojiIndex } from '../src/utils/emoji-data'
+import { EmojiIndex, EmojiView } from '../src/utils/emoji-data'
 import { Emoji, NimbleEmoji } from '../src/components'
 
 describe('Emoji', () => {
+  const index = new EmojiIndex(data)
   const emoji = mount(Emoji, {
     propsData: {
       emoji: 'point_up',
@@ -25,4 +26,34 @@ describe('Emoji', () => {
     emoji.setProps({ native: true })
     expect(emoji.element).toMatchSnapshot()
   })
+
+  it('has correct emoji and emoji view', () => {
+    let expectedEmoji = index.emoji('point_up')
+    let expectedView = new EmojiView(expectedEmoji, 1, 'apple', true, undefined)
+    expect(emoji.vm.emojiObject).toEqual(expectedEmoji)
+    expect(emoji.vm.view).toEqual(expectedView)
+  })
+})
+
+describe('Emoji native', () => {
+  const index = new EmojiIndex(data)
+  const emoji = mount(Emoji, {
+    propsData: {
+      native: true,
+      emoji: 'point_up',
+    },
+  })
+
+  test('renders correctly native emoji', () => {
+    expect(emoji.element).toMatchSnapshot()
+  })
+
+  it('has correct emoji and emoji view', () => {
+    let expectedEmoji = index.emoji('point_up')
+    let expectedView = new EmojiView(expectedEmoji, 1, 'apple', true, undefined)
+    expect(emoji.vm.emojiObject).toEqual(expectedEmoji)
+    expect(emoji.vm.view).toEqual(expectedView)
+  })
+})
+
 })
