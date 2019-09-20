8184276d09c7466bb09230d7f1c58bad003be23e
Boris Serebrov <serebrov@gmail.com>
2019-09-20 13:45:19 +0300

    Add a comment about Quasar integration

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './app'
 
+// See https://github.com/quasarframework/quasar/issues/1576
+// for the information about Quasar integration into existing app.
 import 'quasar-framework/dist/umd/quasar.mat.css'
 import Quasar from 'quasar-framework/dist/quasar.mat.esm'
 Vue.use(Quasar)
