fb74365226fcee4ada62fe765d242f8906a86527
Boris Serebrov <serebrov@gmail.com>
2019-09-20 13:44:15 +0300

    Add Quasar framework QDialog example

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './app'
 
+import 'quasar-framework/dist/umd/quasar.mat.css'
+import Quasar from 'quasar-framework/dist/quasar.mat.esm'
+Vue.use(Quasar)
+
 new Vue({
-    el: '#app',
-    render: h => h(App)
+	el: '#app',
+	render: (h) => h(App),
 })
