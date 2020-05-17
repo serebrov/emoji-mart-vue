c8627a6f1aec50e9131aa932c43134ec72e81766
Boris Serebrov <serebrov@gmail.com>
2020-05-17 18:12:31 +0300

    Fix the issue with skin and colons not being set.

--- src/utils/emoji-data.js
+++ src/utils/emoji-data.js
@@ -445,6 +445,7 @@ export class EmojiData {
           skinData[k] = variationData[k]
         }
         delete skinData.skin_variations
+        skinData['skin_tone'] = skinIdx
         this._skins.push(new EmojiData(skinData))
       }
     }
