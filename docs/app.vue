d55b8551cfe3a3fbd887b7315a27079e5b83ed41
Boris Serebrov <serebrov@gmail.com>
2020-09-26 20:14:29 +0300

    Add I18n example.

--- docs/app.vue
+++ docs/app.vue
@@ -124,6 +124,12 @@
         Show / hide the flags picker (with v-show)
       </button>
     </div>
+
+    <div class="row"></div>
+    <h2>I18n picker example</h2>
+    <div class="row">
+      <Picker emoji="department_store" :data="indexI18n" :i18n="i18n" />
+    </div>
   </div>
 </template>
 
@@ -166,11 +172,42 @@ let indexFiltered = new EmojiIndex(data, {
   },
 })
 
+const i18n = {
+  categories: {
+    search: '搜索结果',
+    recent: '经常使用',
+    smileys: '心情',
+    people: '人物',
+    nature: '动物 & 大自然',
+    foods: '食物 & 饮料',
+    activity: '活动',
+    places: '旅行 & 地标',
+    objects: '物体',
+    symbols: '符号',
+    flags: '国旗',
+    custom: '自定义',
+  },
+}
+
+const indexI18n = new EmojiIndex(data, {
+  exclude: [
+    'flags',
+    'symbols',
+    'objects',
+    'activity',
+    'foods',
+    'people',
+    'places',
+  ],
+})
+
 export default {
   data() {
     return {
       index: index,
       indexFiltered: indexFiltered,
+      indexI18n: indexI18n,
+      i18n: i18n,
       activeSet: 'native',
       emoji: 'point_up',
       title: 'Pick your emoji…',
